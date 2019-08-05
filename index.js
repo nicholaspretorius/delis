038000eeb03eaac7f2609cf01725fd804425ee3d
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-08-05 13:53:31 +0200

    Fixed port ref.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = NODE_ENV.PORT || 3000;
+const PORT = process.evn.PORT || 3000;
 
 app.get("/ping", (req, res) => {
   res.send({ hello: "world!" });
